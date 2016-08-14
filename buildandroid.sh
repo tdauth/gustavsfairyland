577d4b28b212c9975372bd06cdf4c6293491cd13
barade <barade.barade@web.de>
2016-08-14 18:23:36 +0200

    Create APK only when compiling for Android, added prefix path for Qt

--- buildandroid.sh
+++ buildandroid.sh
@@ -7,5 +7,5 @@ export ANDROID_HOME="$HOME/Android/Sdk"
 export PATH="$ANDROID_HOME/tools:$PATH"
 export PATH="$ANDROID_HOME/platform-tools:$PATH"
 cd ./buildandroid
-cmake -DCMAKE_MODULE_PATH="../cmake;/home/tamino/Qt5.7.0/5.7/android_armv7/lib/cmake" -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_NDK=/home/tamino/android-ndk-r12b -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI="armeabi"  -DANDROID_NATIVE_API_LEVEL="24" ../
+cmake -DCMAKE_MODULE_PATH="../cmake" -DCMAKE_PREFIX_PATH="/home/tamino/Qt5.7.0/5.7/android_armv7/lib/cmake" -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_NDK=/home/tamino/android-ndk-r12b -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI="armeabi"  -DANDROID_NATIVE_API_LEVEL="24" -DQT_QMAKE_EXECUTABLE="/home/tamino/Qt5.7.0/5.7/android_armv7/bin/qmake" ../
 cmake --build .
