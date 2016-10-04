467510fa6081975d09907d403e03674d5fb5f3e1
barade <barade.barade@web.de>
2016-10-04 18:18:31 +0200

    Reduced file size, updated README and removed old code

--- main.cpp
+++ main.cpp
@@ -1,44 +1,9 @@
 #include "fairytale.h"
 
 #include <QtGlobal>
-#include <QtDebug>
 #include <QSplashScreen>
-#include <QtWidgets/QApplication>
+#include <QApplication>
 #include <QTime>
-
-#include <stdio.h>
-#include <stdlib.h>
-
-#ifdef Q_OS_ANDROID
-#include <QtQuick/QQuickView>
-#include <QQuickView>
-#include <QQuickItem>
-#include <QQmlProperty>
-#include <QWidget>
-
-void playAndroidVideo(QWidget *parent)
-{
-	QQuickView *view = new QQuickView();
-	QWidget *window = QWidget::createWindowContainer(view, parent);
-	window->setFocusPolicy(Qt::TabFocus);
-
-	// TEST to check if the widget appears at all
-	QPalette pal;
-	pal.setColor(QPalette::Background, Qt::green);
-	window->setPalette(pal);
-
-	view->setSource(QUrl("qrc:/videoplayer.qml"));
-
-	qDebug() << "QML Errors:";
-	foreach (QQmlError error, view->errors())
-	{
-		qDebug() << error.description();
-	}
-
-	view->show();
-}
-
-#endif
 
 int main(int argc, char** argv)
 {
