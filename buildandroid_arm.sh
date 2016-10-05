cad28560a8718995a32d9a3dba97662b33460f58
barade <barade.barade@web.de>
2016-10-05 22:19:32 +0200

    Added build scripts for architectures

---
+++ buildandroid_arm.sh
@@ -0,0 +1,2 @@
+export HOME_TAMINO="/home/tamino"
+bash ./buildandroid.sh "armeabi-v7a" "22" "$HOME_TAMINO/Qt/5.7/android_armv7" "sdk-android-armv7" "armv7"
---
+++ buildandroid_x86.sh
@@ -0,0 +1,2 @@
+export HOME_TAMINO="/home/tamino"
+bash ./buildandroid.sh "x86" "22" "$HOME_TAMINO/Qt5.7.0/5.7/android_x86/" "sdk-android-x86" "x86"
