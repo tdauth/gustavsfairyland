a8166651bf93288e7c5093cfe70df3cc808a5ec9
barade <barade.barade@web.de>
2016-10-06 01:02:43 +0200

    Test another ABI for the correct dir

--- buildandroid_arm.sh
+++ buildandroid_arm.sh
@@ -1,2 +1,2 @@
 export HOME_TAMINO="/home/tamino"
-bash -x ./buildandroid.sh "armeabi-v7a" "22" "$HOME_TAMINO/Qt/5.7/android_armv7" "sdk-android-armv7" "armv7" "lib_android_arm"
+bash -x ./buildandroid.sh "arm-linux-androideabi" "22" "$HOME_TAMINO/Qt/5.7/android_armv7" "sdk-android-armv7" "armv7" "lib_android_arm"
