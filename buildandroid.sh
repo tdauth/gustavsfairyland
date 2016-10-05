85a227594dc33eac512f30fabcf989b31a4a4fe4
barade <barade.barade@web.de>
2016-10-05 21:26:26 +0200

    Added parameters to build scripts which allow building for different architectures

--- buildandroid.sh
+++ buildandroid.sh
@@ -1,15 +1,53 @@
-if [ ! -d "./buildandroid" ] ; then
-	mkdir ./buildandroid
+#!/bin/bash
+ANDROID_ABI="$1"
+ANDROID_API_LEVEL="$2"
+QT_DIR="$3"
+
+export HOME_TAMINO="/home/tamino"
+
+if [ -z "$ANDROID_ABI" ] ; then
+	ANDROID_ABI="armeabi-v7a" # x86
 fi
+
+if [ -z "$ANDROID_API_LEVEL" ] ; then
+	ANDROID_API_LEVEL="22"
+fi
+
+if [ -z "$QT_DIR" ] ; then
+	QT_DIR="$HOME_TAMINO/Qt/5.7/android_armv7" # "$HOME_TAMINO/Qt5.7.0/5.7/android_x86/"
+fi
+
+# Parameters for build QtAV
+ANDROID_PREFIX="$4"
+ANDROID_TARGET="$5"
+
+if [ -z "$ANDROID_PREFIX" ] ; then
+	ANDROID_PREFIX="sdk-android-armv7" # sdk-android-x86
+fi
+
+if [ -z "$ANDROID_TARGET" ] ; then
+	ANDROID_TARGET="armv7" # x86
+fi
+
+BUILD_DIR="./buildandroid$ANDROID_ABI"
+
+if [ ! -d "$BUILD_DIR" ] ; then
+	mkdir "$BUILD_DIR"
+fi
+
+# -DCMAKE_PREFIX_PATH="$HOME/Qt5.7.0/5.7/android_x86/lib/cmake"
 echo "$HOME"
-export PATH="$HOME/Android/Sdk/tools/:$PATH"
-export ANDROID_HOME="$HOME/Android/Sdk"
+export PATH="$HOME_TAMINO/Android/Sdk/tools/:$PATH"
+export ANDROID_HOME="$HOME_TAMINO/Android/Sdk"
 export PATH="$ANDROID_HOME/tools:$PATH"
 export PATH="$ANDROID_HOME/platform-tools:$PATH"
 export ANT_PATH=$(which ant)
-cd ./buildandroid
+cd "$BUILD_DIR"
 # If build type Debug is used CMAKE_GDBSERVER has to specified for the command android_create_apk()
-cmake -DUSE_COMPRESSED_FILES=1 -DJAVA_HOME="/usr/java/default/" -DANDROID_SDK="$ANDROID_HOME" -DQT_ANDROID_SDK_ROOT="$ANDROID_HOME" -DQT_ANDROID_ANT="$ANT_PATH" -DCMAKE_MODULE_PATH="../cmake" -DCMAKE_PREFIX_PATH="$HOME/Qt5.7.0/5.7/android_x86/lib/cmake" -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_NDK="$HOME/android-ndk-r12b" -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI="x86"  -DANDROID_NATIVE_API_LEVEL="24" ../
+cmake -DUSE_COMPRESSED_FILES=1 -DJAVA_HOME="/usr/java/default/" -DANDROID_SDK="$ANDROID_HOME" -DQT_ANDROID_SDK_ROOT="$ANDROID_HOME" -DQT_ANDROID_ANT="$ANT_PATH" -DCMAKE_MODULE_PATH="../cmake" -DCMAKE_PREFIX_PATH="$QT_DIR/lib/cmake" -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_NDK="$HOME_TAMINO/android-ndk-r12b" -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI="$ANDROID_ABI" -DANDROID_NATIVE_API_LEVEL="$ANDROID_API_LEVEL" ../ \
+# Parameters for QtAV
+-DQT_DIR="$QT_DIR" -DANDROID_PREFIX="$ANDROID_PREFIX" -DANDROID_TARGET="$ANDROID_TARGET"
+
 cmake --build .
 # Manual installation of the packed apk file for the emulator
 echo "Installing the APK file"
--- buildqtavforandroid.sh
+++ buildqtavforandroid.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+QT_DIR="$1"
+ANDROID_PREFIX="$2"
+ANDROID_TARGET="$3"
+
 export HOME_TAMINO="/home/tamino"
+
+if [ -z "$QT_DIR" ] ; then
+	QT_DIR="$HOME_TAMINO/Qt/5.7/android_armv7" # "$HOME_TAMINO/Qt5.7.0"
+fi
+
+if [ -z "$ANDROID_PREFIX" ] ; then
+	ANDROID_PREFIX="sdk-android-armv7" # sdk-android-x86
+fi
+
+if [ -z "$ANDROID_TARGET" ] ; then
+	ANDROID_TARGET="armv7" # x86
+fi
+
 export NDK_ROOT="$HOME_TAMINO/android-ndk-r12b"
 export INSTALL_DIR="../installffmpeg"
 export PROJECT_DIR=$(pwd)
-export QT_PATH="$HOME_TAMINO/Qt5.7.0"
+#export QT_PATH="$HOME_TAMINO/Qt5.7.0"
+export QT_PATH="$QT_DIR"
 
 # TODO build ffmpeg with Android compiler
 # https://github.com/wang-bin/build_ffmpeg/wiki
@@ -13,8 +31,7 @@ if [ ! -d ./build_ffmpeg ] ; then
 fi
 
 export FFSRC="$PROJECT_DIR/ffmpeg-3.1.4/" #/path/to/ffmpeg # if no ffmpeg source fold under this dir
-
-export FFMPEG_PREFIX="$PROJECT_DIR/build_ffmpeg/sdk-android-x86/"
+export FFMPEG_PREFIX="$PROJECT_DIR/build_ffmpeg/$ANDROID_PREFIX/"
 
 # Download and extract ffmpeg if it does not exist.
 # TODO checksums? or rather use a local copy
@@ -27,7 +44,7 @@ fi
 cp -f "$PROJECT_DIR/config-android.sh" ./build_ffmpeg
 # TODO specify NDK_ROOT in file "config-android.sh" as well as static build
 cd ./build_ffmpeg
-./build_ffmpeg.sh android x86
+./build_ffmpeg.sh android "$ANDROID_TARGET"
 
 # Change back to the project dir
 cd ..
@@ -53,27 +70,27 @@ fi
 cd ./buildqtav
 #export CPATH="$PROJECT_DIR/ffmpeg-3.1.1-android/include/":openal_path/include:$CPATH
 #export LIBRARY_PATH="$PROJECT_DIR/ffmpeg-3.1.1-android/lib/armv7":openal_path/lib:$LIBRARY_PATH
-export CPATH="$PROJECT_DIR/build_ffmpeg/sdk-android-x86/include/:$CPATH"
+export CPATH="$FFMPEG_PREFIX/include/:$CPATH"
 export FFMPEG_LIB_DIR="$FFMPEG_PREFIX/lib"
 export LIBRARY_PATH="$FFMPEG_LIB_DIR:$LIBRARY_PATH"
 export LD_LIBRARY_PATH="$FFMPEG_LIB_DIR:$LD_LIBRARY_PATH"
 export ANDROID_NDK_ROOT="$NDK_ROOT"
 
-for f in "$PROJECT_DIR/build_ffmpeg/sdk-android-x86/include/"* ; do
-	echo "Creating link for $f"
-	target="$QT_PATH/5.7/android_x86/include"
+for f in "$FFMPEG_PREFIX/include/"* ; do
+	target="$QT_PATH/include"
+	echo "Creating link for $f to $target"
 
 	if [ ! -e "$target" ] ; then
-		ln -s "$f" "$target"
+		ln -fs "$f" "$target"
 	fi
 done
 
 for f in "$FFMPEG_LIB_DIR/"* ; do
-	echo "Creating link for $f"
-	target="$QT_PATH/5.7/android_x86/lib"
+	target="$QT_PATH/lib"
+	echo "Creating link for $f to $target"
 
 	if [ ! -e "$target" ] ; then
-		ln -s "$f" "$target"
+		ln -fs "$f" "$target"
 	fi
 done
 
@@ -82,6 +99,7 @@ echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
 echo "ls $FFMPEG_LIB_DIR/:"
 ls "$FFMPEG_LIB_DIR/"
 echo "Building QtAV from: $(pwd)"
+echo "Running qmake: \"$QT_PATH/bin/qmake\""
 # "$QT_PATH/5.7/android_x86/bin/qmake"
 # += staticlib to build itself statically
 # "CONFIG += static_ffmpeg static_openal"
@@ -89,5 +107,6 @@ echo "Building QtAV from: $(pwd)"
 # -L$FFMPEG_LIB_DIR/
 # -r "CONFIG+=recheck"
 # "CONFIG += no_config_tests" NOTE Don't use this, it leads to errors.
-"$QT_PATH/5.7/android_x86/bin/qmake" -Wall "LIBS+=-L$QT_PATH/5.7/android_x86/lib -L$FFMPEG_LIB_DIR" ../qtav/QtAV.pro
+# Take off config_vaapi  for ARM?
+"$QT_PATH/bin/qmake" -Wall "LIBS+=-L$QT_PATH/lib -L$FFMPEG_LIB_DIR" ../qtav/QtAV.pro
 make -j4
