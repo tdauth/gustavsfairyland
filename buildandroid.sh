c8eb903fa84fada352555aff904bf93797f03593
barade <barade.barade@web.de>
2016-10-06 01:07:05 +0200

    Define ANDROID_TOOLCHAIN_NAME to fix ARM build

--- buildandroid.sh
+++ buildandroid.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 ANDROID_ABI="$1"
-ANDROID_API_LEVEL="$2"
-QT_DIR="$3"
+ANDROID_TOOLCHAIN_NAME="$2"
+ANDROID_API_LEVEL="$3"
+QT_DIR="$4"
 
 export HOME_TAMINO="/home/tamino"
 
 if [ -z "$ANDROID_ABI" ] ; then
 	echo "Setting ANDROID_ABI automatically"
 	ANDROID_ABI="armeabi-v7a" # x86
+fi
+
+if [ -z "$ANDROID_TOOLCHAIN_NAME" ] ; then
+	echo "Setting ANDROID_TOOLCHAIN_NAME automatically"
+	ANDROID_TOOLCHAIN_NAME="arm-linux-androideabi-4.9" # "x86-4.9"
 fi
 
 if [ -z "$ANDROID_API_LEVEL" ] ; then
@@ -21,9 +27,9 @@ if [ -z "$QT_DIR" ] ; then
 fi
 
 # Parameters for build QtAV
-ANDROID_PREFIX="$4"
-ANDROID_TARGET="$5"
-QTAV_LIB_DIR="$6"
+ANDROID_PREFIX="$5"
+ANDROID_TARGET="$6"
+QTAV_LIB_DIR="$7"
 
 if [ -z "$ANDROID_PREFIX" ] ; then
 	echo "Setting ANDROID_PREFIX automatically"
@@ -53,7 +59,7 @@ fi
 cd "$BUILD_DIR"
 echo "Build dir: $(pwd)"
 # If build type Debug is used CMAKE_GDBSERVER has to specified for the command android_create_apk()
-cmake -DUSE_COMPRESSED_FILES=1 -DJAVA_HOME="/usr/java/default/" -DANDROID_SDK="$ANDROID_HOME" -DQT_ANDROID_SDK_ROOT="$ANDROID_HOME" -DQT_ANDROID_ANT="$ANT_PATH" -DCMAKE_MODULE_PATH="../cmake" -DCMAKE_PREFIX_PATH="$QT_DIR/lib/cmake" -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_NDK="$HOME_TAMINO/android-ndk-r12b" -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI="$ANDROID_ABI" -DANDROID_NATIVE_API_LEVEL="$ANDROID_API_LEVEL" -DQT_DIR="$QT_DIR" -DANDROID_PREFIX="$ANDROID_PREFIX" -DANDROID_TARGET="$ANDROID_TARGET" -DQTAV_LIB_DIR="$QTAV_LIB_DIR" ../
+cmake -DUSE_COMPRESSED_FILES=1 -DJAVA_HOME="/usr/java/default/" -DANDROID_SDK="$ANDROID_HOME" -DQT_ANDROID_SDK_ROOT="$ANDROID_HOME" -DQT_ANDROID_ANT="$ANT_PATH" -DCMAKE_MODULE_PATH="../cmake" -DCMAKE_PREFIX_PATH="$QT_DIR/lib/cmake" -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_NDK="$HOME_TAMINO/android-ndk-r12b" -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI="$ANDROID_ABI" -DANDROID_TOOLCHAIN_NAME="$ANDROID_TOOLCHAIN_NAME" -DANDROID_NATIVE_API_LEVEL="$ANDROID_API_LEVEL" -DQT_DIR="$QT_DIR" -DANDROID_PREFIX="$ANDROID_PREFIX" -DANDROID_TARGET="$ANDROID_TARGET" -DQTAV_LIB_DIR="$QTAV_LIB_DIR" ../
 
 cmake --build .
 # Manual installation of the packed apk file for the emulator
