0e3a589d3da3a87b75ccd2938c834a05ec1b7b63
barade <barade.barade@web.de>
2016-10-06 00:30:19 +0200

    Simplified build script and hopefully fixed dependencies to generated libraries

--- buildandroid.sh
+++ buildandroid.sh
@@ -34,22 +34,20 @@ if [ -z "$ANDROID_TARGET" ] ; then
 	ANDROID_TARGET="armv7" # x86
 fi
 
+export ANDROID_HOME="$HOME_TAMINO/Android/Sdk"
+export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH"
+export ANT_PATH=$(which ant)
+
 BUILD_DIR="./buildandroid$ANDROID_ABI"
 
 if [ ! -d "$BUILD_DIR" ] ; then
 	mkdir "$BUILD_DIR"
 fi
 
-# -DCMAKE_PREFIX_PATH="$HOME/Qt5.7.0/5.7/android_x86/lib/cmake"
-echo "$HOME"
-export PATH="$HOME_TAMINO/Android/Sdk/tools/:$PATH"
-export ANDROID_HOME="$HOME_TAMINO/Android/Sdk"
-export PATH="$ANDROID_HOME/tools:$PATH"
-export PATH="$ANDROID_HOME/platform-tools:$PATH"
-export ANT_PATH=$(which ant)
 cd "$BUILD_DIR"
+echo "Build dir: $(pwd)"
 # If build type Debug is used CMAKE_GDBSERVER has to specified for the command android_create_apk()
-cmake -DUSE_COMPRESSED_FILES=1 -DJAVA_HOME="/usr/java/default/" -DANDROID_SDK="$ANDROID_HOME" -DQT_ANDROID_SDK_ROOT="$ANDROID_HOME" -DQT_ANDROID_ANT="$ANT_PATH" -DCMAKE_MODULE_PATH="../cmake" -DCMAKE_PREFIX_PATH="$QT_DIR/lib/cmake" -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_NDK="$HOME_TAMINO/android-ndk-r12b" -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI="$ANDROID_ABI" -DANDROID_NATIVE_API_LEVEL="$ANDROID_API_LEVEL" ../ -DQT_DIR="$QT_DIR" -DANDROID_PREFIX="$ANDROID_PREFIX" -DANDROID_TARGET="$ANDROID_TARGET"
+cmake -DUSE_COMPRESSED_FILES=1 -DJAVA_HOME="/usr/java/default/" -DANDROID_SDK="$ANDROID_HOME" -DQT_ANDROID_SDK_ROOT="$ANDROID_HOME" -DQT_ANDROID_ANT="$ANT_PATH" -DCMAKE_MODULE_PATH="../cmake" -DCMAKE_PREFIX_PATH="$QT_DIR/lib/cmake" -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_NDK="$HOME_TAMINO/android-ndk-r12b" -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI="$ANDROID_ABI" -DANDROID_NATIVE_API_LEVEL="$ANDROID_API_LEVEL" -DQT_DIR="$QT_DIR" -DANDROID_PREFIX="$ANDROID_PREFIX" -DANDROID_TARGET="$ANDROID_TARGET" ../
 
 cmake --build .
 # Manual installation of the packed apk file for the emulator
