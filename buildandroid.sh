7b90f1591deb682c405ecf3ea945c522fd90abd7
barade <barade.barade@web.de>
2016-10-06 02:40:29 +0200

    Fixed Qt toolchain prefix for CMake, make CMake build verbose for debugging

--- buildandroid.sh
+++ buildandroid.sh
@@ -14,11 +14,12 @@ fi
 mkdir "$BUILD_DIR"
 cd "$BUILD_DIR"
 echo "Build dir: $(pwd)"
-# If build type Debug is used CMAKE_GDBSERVER has to specified for the command android_create_apk()
-cmake -DUSE_COMPRESSED_FILES=1 -DJAVA_HOME="/usr/java/default/" -DANDROID_SDK="$ANDROID_HOME" -DQT_ANDROID_SDK_ROOT="$ANDROID_HOME" -DQT_ANDROID_ANT="$ANT_PATH" -DCMAKE_MODULE_PATH="../cmake" -DCMAKE_PREFIX_PATH="$QT_DIR/lib/cmake" -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_NDK="$HOME_TAMINO/android-ndk-r12b" -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI="$ANDROID_ABI" -DANDROID_TOOLCHAIN_NAME="$ANDROID_TOOLCHAIN_NAME" -DANDROID_STANDALONE_TOOLCHAIN="$ANDROID_STANDALONE_TOOLCHAIN" -DANDROID_NATIVE_API_LEVEL="$ANDROID_API_LEVEL" -DQT_DIR="$QT_DIR" -DANDROID_PREFIX="$ANDROID_PREFIX" -DANDROID_TARGET="$ANDROID_TARGET" -DQTAV_LIB_DIR="$QTAV_LIB_DIR" ../
 
-cmake --build .
-# Manual installation of the packed apk file for the emulator
+# If build type Debug is used CMAKE_GDBSERVER has to specified for the command android_create_apk()
+cmake -DCMAKE_VERBOSE_MAKEFILE=on -DUSE_COMPRESSED_FILES=1 -DJAVA_HOME="/usr/java/default/" -DANDROID_SDK="$ANDROID_HOME" -DQT_ANDROID_SDK_ROOT="$ANDROID_HOME" -DQT_ANDROID_ANT="$ANT_PATH" -DCMAKE_MODULE_PATH="../cmake" -DCMAKE_PREFIX_PATH="$QT_DIR/lib/cmake" -DCMAKE_TOOLCHAIN_FILE=../cmake/android.toolchain.cmake -DANDROID_NDK="$HOME_TAMINO/android-ndk-r12b" -DCMAKE_BUILD_TYPE=Debug -DANDROID_ABI="$ANDROID_ABI" -DANDROID_TOOLCHAIN_NAME="$ANDROID_TOOLCHAIN_NAME" -DANDROID_NATIVE_API_LEVEL="$ANDROID_API_LEVEL" -DQT_DIR="$QT_DIR" -DANDROID_PREFIX="$ANDROID_PREFIX" -DANDROID_TARGET="$ANDROID_TARGET" -DQTAV_LIB_DIR="$QTAV_LIB_DIR" ../
+make VERBOSE=1
+
+# Manual installation of the packed APK file for the emulator. There is an option for add_qt_android_apk() as well.
 echo "Installing the APK file"
 $ANDROID_HOME/platform-tools/adb install -r ./bin/QtApp-debug.apk
 #$ANDROID_HOME/platform-tools/adb install -r ./binQtApp-release-signed.apk
--- buildandroid_arm.sh
+++ buildandroid_arm.sh
@@ -2,9 +2,8 @@ export HOME_TAMINO="/home/tamino"
 
 export ANDROID_ABI="armeabi-v7a" # x86
 export ANDROID_TOOLCHAIN_NAME="arm-linux-androideabi-4.9" # "x86-4.9"
-export ANDROID_API_LEVEL="22"
+export ANDROID_API_LEVEL="23"
 export QT_DIR="$HOME_TAMINO/Qt/5.7/android_armv7" # "$HOME_TAMINO/Qt5.7.0/5.7/android_x86/"
-export ANDROID_STANDALONE_TOOLCHAIN="$HOME_TAMINO/android-ndk-r12b/toolchains/arm-linux-androideabi-4.9/"
 export ANDROID_PREFIX="sdk-android-armv7" # sdk-android-x86
 export ANDROID_TARGET="armv7" # x86
 export QTAV_LIB_DIR="lib_android_arm" # lib_android_x86
