e3ce084366d1b325bbd20c37890fcfe6e1202913
barade <barade.barade@web.de>
2016-10-16 16:44:04 +0200

    Added Linux build script

---
+++ buildlinux_debug.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# The Jenkins build script for Linux.
+# Copy clips file which are not in the GIT repository
+cp -f "$WORKSPACE/fairytale/clips/gustav.xml" "$WORKSPACE/gustav.xml.tmp"
+cp -rf "/home/tamino/Dokumente/Projekte/fairytale/clips/*" "$WORKSPACE/fairytale/clips/"
+cp -f "$WORKSPACE/gustav.xml.tmp" "$WORKSPACE/fairytale/clips/gustav.xml"
+cd "$WORKSPACE/build"
+make -j4
+ctest -T "Test" || true
