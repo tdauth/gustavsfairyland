b2dd0e3856a24364083283a259e588447c83574a
barade <barade.barade@web.de>
2016-10-16 17:13:16 +0200

    Configure CMake after copying clips

--- buildlinux_debug.sh
+++ buildlinux_debug.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # The Jenkins build script for Linux.
 bash -x copyclipsforbuild.sh
+cd "$WORKSPACE/fairytale"
+mkdir "$WORKSPACE/build"
 cd "$WORKSPACE/build"
+cmake ../ -DUSE_COMPRESSED_FILES=${USE_COMPRESSED_FILES} -DCMAKE_BUILD_TYPE="Debug"
 make -j4
 ctest -T "Test" || true
--- buildlinux_release.sh
+++ buildlinux_release.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # The Jenkins build script for Linux.
 bash -x copyclipsforbuild.sh
+cd "$WORKSPACE/fairytale"
+mkdir "$WORKSPACE/build"
 cd "$WORKSPACE/build"
+cmake ../ -DUSE_COMPRESSED_FILES=${USE_COMPRESSED_FILES} -DCMAKE_BUILD_TYPE="Release"
 make -j4
