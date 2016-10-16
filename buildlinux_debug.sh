aa94eec5efa7fd40520bc6778c40ee06bbce3778
barade <barade.barade@web.de>
2016-10-16 16:56:42 +0200

    Added copyclips script

--- buildlinux_debug.sh
+++ buildlinux_debug.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # The Jenkins build script for Linux.
-# Copy clips file which are not in the GIT repository
-cp -f "$WORKSPACE/fairytale/clips/gustav.xml" "$WORKSPACE/gustav.xml.tmp"
-cp -rf "/home/tamino/Dokumente/Projekte/fairytale/clips/"* "$WORKSPACE/fairytale/clips/"
-cp -f "$WORKSPACE/gustav.xml.tmp" "$WORKSPACE/fairytale/clips/gustav.xml"
+bash copyclipsforbuild.sh
 cd "$WORKSPACE/build"
 make -j4
 ctest -T "Test" || true
