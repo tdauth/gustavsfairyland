1359630fdd1d43c1d6d854d0c754d4cd4931e1e0
barade <barade.barade@web.de>
2016-12-08 08:32:08 +0100

    Fixed building QtMEL

--- buildffmpeg_qtmel.sh
+++ buildffmpeg_qtmel.sh
@@ -8,10 +8,9 @@ BUILD_DIR="$2"
 if [ ! -e "$BUILD_DIR" ] ; then
 	mkdir -p "$BUILD_DIR"
 	cd "$BUILD_DIR"
-	tar -jxf ${CMAKE_SOURCE_DIR}/ffmpeg-git-14d94a1.tar.bz2
-else
-	cd "$BUILD_DIR"
+	tar -jxf "$TAR_FILE"
 fi
 
+cd "$BUILD_DIR/ffmpeg-git-14d94a1"
 ./configure --prefix="$BUILD_DIR"/install
 make -j4 install
