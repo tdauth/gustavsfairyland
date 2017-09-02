09a2542aa15c50a0b7a544369b4a1e0ce5c4eaaa
tdauth <tamino@cdauth.de>
2017-09-02 14:20:32 +0200

    Add utilities header and update README path

---
+++ src/utilities.h
@@ -0,0 +1,15 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <random>
+
+int randomNumber(int min, int max)
+{
+	static std::random_device rd;
+	static std::mt19937 eng(rd());
+	std::uniform_int_distribution<> distr(min, max);
+
+	return distr(eng);
+}
+
+#endif
